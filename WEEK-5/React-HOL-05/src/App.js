78b9496223b9fc28bb0c9ecd75ccf9a9bfbb3cfb
Amit Goyal <amitpvt0150@gmail.com>
2026-07-23 23:05:39 +0530

    Week - 5 Completed

---
+++ WEEK-5/React-HOL-05/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React HOL 05</h1>
+      <p>This React hand-on project was created for the document in this folder.</p>
+      <button>Start building</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ WEEK-5/React-HOL-13/README.md
@@ -0,0 +1,9 @@
+# React HOL 13
+
+This folder contains a minimal React project scaffold for the hands-on document inside this directory.
+
+## Run locally
+
+1. Open this folder in a terminal.
+2. Run `npm install`.
+3. Run `npm start`.
